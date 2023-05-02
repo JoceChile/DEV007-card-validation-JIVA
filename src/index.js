8afeedc539ee38df7b663b33891714d35eed5688
Jocelyn <129541722+JoceChile@users.noreply.github.com>
2023-05-01 21:27:49 -0400

    pruebas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import validator from './validator.js';
 
-console.log(validator);
+const tarjetaInput = document.getElementById("card-number");// tarjetaInput llama a mi id card-number que es el cuadrado donde se apuntan los numeros
+const numerosDeTarjeta = document.getElementById("boton-enviar");// numerosDeTarjeta llama al boton de validar que su id es boton-enviar
+numerosDeTarjeta.addEventListener("click", () => {
+  //console.log(tarjetaInput.value);// cuando se escuche el click el boton hace que me den los numeros del tarjetaInput
+  if (tarjetaInput.value === "") {
+    alert("Estimad@ Client@, debe ingresar todos los datos")
+    return 
+  }
+  const codigo = document.getElementById("cvv");
+  const fecha = document.getElementById("expiracion")
+  if (codigo.value === "") {
+    alert("Estimad@ Client@, debe ingresar todos los datos")
+    return 
+  }
+  if (fecha.value === "") {
+    alert("Estimad@ Client@, debe ingresar todos los datos")
+    return 
+  }
+
+  const resultado = validator.isValid(tarjetaInput.value);// le decimos que el numero a validar seran los numeros ingresados en el tarjetaInput
+  
+  const enmascarar = validator.maskify(tarjetaInput.value);//le decimos que el numero a enmascarar seran los numeros ingresados en el tarjetaInput Valido
+  const maskify = enmascarar;
+  document.getElementById("card-number").innerHTML;
+
+  if (resultado === true) {
+    alert("Tarjeta " + maskify + " Válida. Muchas gracias por su compra.")
+  } else {
+    alert("Tarjeta No Válida, por favor intente con otra tarjeta u otro medio de pago.")
+  } 
+   
+
+}
+);
+const reload =document.getElementById("reload");
+reload.addEventListener("click", () => {
+  location.reload();
+});// esta funcion es para utilizar otra tarjeta se borra toda la información
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,41 @@
 const validator = {
-  // ...
+  isValid: function(validarTarjeta) {
+    //console.log(validarTarjeta);// aqui recibo los numeros ingresados
+    const convertirStringArray = validarTarjeta.split("").reverse().join("");
+    //console.log(convertirStringArray);// convierto el string en array y lo reverso
+    let totalSumas = 0;
+    for (let i = 0; i < convertirStringArray.length; i++) {// aqui estoy buscando las posiciones
+      let numerosPares;
+      let unDigito;
+      if ((i + 1) % 2 === 0) {//posiciones array
+        numerosPares = (2 * parseInt(convertirStringArray[i]));// multiplicar por dos los pares
+        //console.log(numerosPares);// voy bien, se ven todos los pares multiplicados por dos en consola
+        if (numerosPares >= 10) {
+          unDigito = numerosPares - 9;
+          //console.log(unDigito);// los pares mayores a diez se les resta nueve y quedan de un digito, los pares normales quedan igual
+          totalSumas += unDigito;
+        } else {
+          totalSumas += numerosPares;// aqui acumulo los pares ya solo de un digito
+        }
+      }
+      else {
+        totalSumas += parseInt(convertirStringArray[i])
+      }
+      //console.log(totalSumas);//suma total array
+    }
+    if (totalSumas % 10 === 0) {//defino si es valida o no
+      return true;
+    } else {
+      return false;
+    }
+  }, 
+  
+  maskify: function (tarjetaInput) {
+    const ocultar= tarjetaInput.replace(/.(?=.{4})/g, "#");
+    return ocultar
+  }
+ 
+
 };
 
 export default validator;
